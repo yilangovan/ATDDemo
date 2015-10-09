a8e09a9da4f81fba7e337c0fceaf903e78ff94c2
Yuvaraj Ilangovan <yilangovan@agiletrailblazers.com>
2015-10-09 16:08:54 -0400

    changes are commited

--- src/test/java/com/cucumber/CucumberMavenPrototype/AbstractSteps.java
+++ src/test/java/com/cucumber/CucumberMavenPrototype/AbstractSteps.java
@@ -44,8 +44,8 @@ public class AbstractSteps {
 	            driver = new OperaDriver();
 	        }
 	        else if(browser.equalsIgnoreCase("chrome")){
-	      System.setProperty("webdriver.chrome.driver","/Users/yuvarajilangovan/Desktop/chromedriver1");
-	        	//System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+	      //System.setProperty("webdriver.chrome.driver","/Users/yuvarajilangovan/Desktop/chromedriver1");
+	        	System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver1");
 	 
 	      //create chrome instance
 	      driver = new ChromeDriver();
